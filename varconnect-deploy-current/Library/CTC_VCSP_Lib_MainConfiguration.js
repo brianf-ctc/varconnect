d53d071349e406a5a6a3ba91e36895b0edf61e36
Brian Feliciano <brianf@nscatalyst.com>
2021-10-21 08:38:10 +0800

    varconnect-deploy files

---
+++ varconnect-deploy-current/Library/CTC_VCSP_Lib_MainConfiguration.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2017 Catalyst Tech Corp
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Catalyst Tech Corp. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Catalyst Tech.
+ */
+
+/**
+ * Module Description
+ *
+ * Version	Date            Author		Remarks
+ * 1.00		Mar 09, 2020	paolodl		Library for retrieving Main Configuration record
+ * 
+ *//**
+ * @NApiVersion 2.x
+ * @NModuleScope SameAccount
+ */
+define(['N/search', './CTC_VCSP_Lib_Preferences', './CTC_VCSP_Constants.js'],
+
+function(search, pref, constants) {
+	var mainConfigFields = [constants.Fields.MainConfig.ID,										//0
+	                        constants.Fields.MainConfig.LICENSE									//1
+	                        ];
+	
+	function _generateMainConfig(recLookup) {
+		return {
+			id: recLookup[mainConfigFields[0]],
+			license: recLookup[mainConfigFields[1]]
+		};
+	}
+	
+	function getMainConfiguration() {
+		var result = {};
+		
+		var recLookup = search.lookupFields({
+			type: constants.Records.MAIN_CONFIG,
+			id: 1,
+			columns: mainConfigFields
+		});
+		
+		if (recLookup) {
+			result = _generateMainConfig(recLookup);
+		}
+		
+		return result;
+	}
+   
+    return {
+        getMainConfiguration: getMainConfiguration
+    };
+    
+});
---
+++ varconnect-deploy-current/com.catalysttechcorp.varconnect/Bill Creator/Libraries/CTC_VC_Lib_Suitelet_Client_Script.js
@@ -0,0 +1,95 @@
+define(['N/ui/dialog', 'N/ui/message', 'N/currentRecord'], function (dialog, message, currentRecord) {
+    /**
+     *@NApiVersion 2.0
+     *@NScriptType ClientScript
+     */
+
+    function fieldChanged(context) {
+        var currentValue = context.currentRecord.getValue(context);
+
+        if (context.fieldId == 'custpage_polink' && nlapiGetFieldValue('custpage_polink') !== '') {
+            dialog
+                .confirm({
+                    title: 'WARNING',
+                    message:
+                        'You are manually linking a PO.  ' +
+                        'Once this record is saved with a linked PO, ' +
+                        'the PO can not be changed.  ' +
+                        'This Bill will have to be closed and manually ' +
+                        'created if linked to an incorrect PO!'
+                })
+                .then(function (result) {
+                    if (result == false) {
+                        nlapiSetFieldValue('custpage_polink', '');
+                    }
+                })
+                .catch(function (reason) {
+                    console.log('Failure: ' + reason);
+                });
+        }
+
+        if (context.fieldId == 'custpage_action') {
+            var currRecord = context.currentRecord;
+            // console.log('custpage_action', currentValue);
+
+            var taxValue = currRecord.getValue('custpage_variance_tax');
+            var shipValue = currRecord.getValue('custpage_variance_shipping');
+            var otherValue = currRecord.getValue('custpage_variance_other');
+
+            // console.log('custpage_action', taxValue, shipValue, otherValue);
+
+            if (currentValue == 'reprocess_hasvar') {
+                currRecord.setValue({ fieldId: 'custpage_variance_tax_apply', value: !!taxValue });
+                currRecord.setValue({ fieldId: 'custpage_variance_ship_apply', value: !!shipValue });
+                currRecord.setValue({ fieldId: 'custpage_variance_other_apply', value: !!otherValue });
+            } else if (currentValue == 'reprocess_novar') {
+                currRecord.setValue({ fieldId: 'custpage_variance_tax_apply', value: false });
+                currRecord.setValue({ fieldId: 'custpage_variance_ship_apply', value: false });
+                currRecord.setValue({ fieldId: 'custpage_variance_other_apply', value: false });
+            }
+        }
+
+        var fldAmount;
+        if (context.fieldId == 'custpage_variance_tax_apply') {
+            fldAmount = context.currentRecord.getField('custpage_variance_tax');
+            fldAmount.isDisabled = !currentValue;
+        }
+        if (context.fieldId == 'custpage_variance_ship_apply') {
+            fldAmount = context.currentRecord.getField('custpage_variance_shipping');
+            fldAmount.isDisabled = !currentValue;
+        }
+        if (context.fieldId == 'custpage_variance_other_apply') {
+            fldAmount = context.currentRecord.getField('custpage_variance_other');
+            fldAmount.isDisabled = !currentValue;
+        }
+    }
+
+    function pageInit(context) {
+        var currRecord = context.currentRecord;
+        // console.log('currRecord', currRecord);
+
+        var taxValueField = currRecord.getField('custpage_variance_tax');
+        taxValueField.isDisabled = !currRecord.getValue('custpage_variance_tax_apply');
+        // console.log('tax', taxValueField, currRecord.getValue('custpage_variance_tax_apply'));
+
+        var shipValueField = currRecord.getField('custpage_variance_shipping');
+        shipValueField.isDisabled = !currRecord.getValue('custpage_variance_ship_apply');
+        // console.log('shipValueField', shipValueField, currRecord.getValue('custpage_variance_ship_apply'));
+
+        var otherValueField = currRecord.getField('custpage_variance_other');
+        otherValueField.isDisabled = !currRecord.getValue('custpage_variance_other_apply');
+        // console.log('otherValueField', otherValueField, currRecord.getValue('custpage_variance_other_apply'));
+
+        return true;
+    }
+
+    return {
+        fieldChanged: fieldChanged,
+        pageInit: pageInit, 
+        goToBillFile: function () {
+            var curRec = currentRecord.get();
+            var url = curRec.getValue({fieldId: 'custpage_bill_file'});
+            return window.open(url, '_blank');
+        }
+    };
+});
