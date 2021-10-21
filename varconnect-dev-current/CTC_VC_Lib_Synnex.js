2fc65bc7676598a7cb9c587a394b593afbd31352
Brian Feliciano <brianf@nscatalyst.com>
2021-10-21 08:38:24 +0800

    varconnect-dev files

---
+++ varconnect-dev-current/CTC_VC_Lib_Synnex.js
@@ -0,0 +1,215 @@
+/**
+ * Copyright (c) 2017 Catalyst Tech Corp
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Catalyst Tech Corp. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Catalyst Tech.
+ */
+
+/**
+ * Module Description
+ *
+ * Version	Date            Author		Remarks
+ * 1.00		July 25, 2019	paolodl		Library for retrieving Vendor Configuration
+ * 
+ *//**
+ * @NApiVersion 2.x
+ * @NModuleScope SameAccount
+ */
+define(['N/search', 
+        'N/runtime', 
+        'N/record', 
+        'N/xml',
+        'N/https', 
+        './VC_Globals.js', 
+        './CTC_VC_Constants.js', 
+        './CTC_VC_Lib_Log.js'],
+function(search, 
+		runtime, 
+		r, 
+		xml, 
+		https, 
+		vcGlobals, 
+		constants, 
+		vcLog) {
+	function processRequest(options) {
+		var poNum = options.poNum,
+            poId = options.poId,
+			vendorConfig = options.vendorConfig,
+			requestURL = vendorConfig.endPoint,
+			userName = vendorConfig.user,
+			password = vendorConfig.password,
+			customerNo = vendorConfig.customerNo;
+		
+        var orderXMLLineData = [];
+
+        var xmlorderStatus =
+            '<?xml version="1.0" encoding="UTF-8" ?>' +
+            '<SynnexB2B version="2.2">' +
+            '<Credential>' +
+            '<UserID>' + userName + '</UserID>' +
+            '<Password>' + password + '</Password>' +
+            '</Credential>' +
+            '<OrderStatusRequest>' +
+            '<CustomerNumber>' + customerNo + '</CustomerNumber>' +
+            '<PONumber>' + poNum + '</PONumber>' +
+            '</OrderStatusRequest>' +
+            '</SynnexB2B>';
+
+        var headers = {
+            'Content-Type': 'text/xml; charset=utf-8',
+            'Content-Length': 'length'
+        };
+
+        vcLog.recordLog({
+            header: "Synnex Request",
+            body: xmlorderStatus,
+            transaction: poId,
+            status: constants.Lists.VC_LOG_STATUS.INFO
+        });
+
+        
+        var responseXML;
+        log.debug('prerequest ' + poNum);
+        try {
+            var response = https.post({
+                url: requestURL,
+                body: xmlorderStatus,
+                headers: headers
+            });
+            responseXML = response.body;
+            log.debug({
+                title: 'Synnex Scheduled',
+                details: 'length of response '+responseXML.length
+            });
+
+        }
+        catch (err) {
+            log.error({
+                title: 'Synnex scheduled',
+                details: 'error = '+err.message
+            });
+            responseXML = null;
+        }
+
+        return responseXML;
+	}
+	
+	function processResponse(options) {
+		var xmlString = options.responseXML;
+        log.audit('parseSynnex', xmlString);
+
+        try{
+            var itemArray = [];
+            var xmlDoc = xml.Parser.fromString({
+                text : xmlString
+            });
+
+            var itemNodesArray = xmlDoc.getElementsByTagName({tagName:'Item'});
+            var orderDateTime = xmlDoc.getElementsByTagName({tagName:'PODatetime'});
+
+            for (var i = 0; i < itemNodesArray.length; i++) {
+                var itemRow = {line_num:"NA", item_num:"NA", order_num:"NA", order_date:"NA", order_eta:"NA", ship_qty:"NA", ship_date:"NA", tracking_num:"NA", vendorSKU: "NA", carrier:"NA", serial_num:"NA"};
+                var itemCode = '';
+
+                var itemNode = itemNodesArray[i];
+
+                itemRow.line_num = itemNode.getAttribute({name: 'lineNumber'});
+                itemRow.order_date = orderDateTime[0].textContent;
+
+                var itemChildNodes = itemNode.childNodes;
+                var packageNodes;
+                for (var j = 0; j < itemChildNodes.length; j++) {
+                    switch (itemChildNodes[j].nodeName) {
+                        case 'Code' :					itemCode = itemChildNodes[j].textContent;
+                            break;
+                        case 'OrderNumber' : 			itemRow.order_num = itemChildNodes[j].textContent;
+                            break;
+                        case 'MfgPN' : 					itemRow.item_num = itemChildNodes[j].textContent;
+                            break;
+                        case 'ShipDatetime' : 			itemRow.ship_date = itemChildNodes[j].textContent;
+                            break;
+                        case 'SKU' : 		        	itemRow.vendorSKU = itemChildNodes[j].textContent;
+                            break;
+                        case 'ShipMethodDescription' : 	itemRow.carrier = itemChildNodes[j].textContent;
+                            break;
+                        case 'ShipQuantity' : 			itemRow.ship_qty = itemChildNodes[j].textContent;
+                            break;
+                        case 'ETADate' : 				itemRow.order_eta = itemChildNodes[j].textContent;
+                        	break;
+                        case 'Packages' : 				packageNodes = itemChildNodes[j].childNodes;
+                            for (var x = 0; x < packageNodes.length; x++) {
+                                if (packageNodes[x].nodeName == 'Package') {
+                                    var packageChildNodes = packageNodes[x].childNodes;
+                                    for (var z = 0; z < packageChildNodes.length; z++) {
+                                        switch (packageChildNodes[z].nodeName) {
+                                            case 'TrackingNumber' :	if (itemRow.tracking_num === 'NA')
+                                                itemRow.tracking_num = packageChildNodes[z].textContent;
+                                            else
+                                                itemRow.tracking_num += ',' + packageChildNodes[z].textContent;
+                                                break;
+                                            case 'SerialNo' : 	if (itemRow.serial_num === 'NA')
+                                                itemRow.serial_num = packageChildNodes[z].textContent;
+                                            else
+                                                itemRow.serial_num += ',' + packageChildNodes[z].textContent;
+                                                break;
+                                        };
+                                    };
+                                };
+                            };
+                            break;
+                    }
+                }
+
+                // ignore items unles they have been invoiced
+                if (itemCode == 'invoiced') {
+                    itemArray.push(itemRow);
+                }
+            }
+        }
+        catch (err) {
+            log.error({
+                title: 'Synnex Parse ERRROR',
+                details: 'error = '+err.message
+            });
+
+        }
+        log.debug('exiting Parse Synnex', itemArray);
+        return itemArray;
+	}
+	
+	function process(options) {
+		var poNum = options.poNum,
+			poId = options.poId,
+			vendorConfig = options.vendorConfig,
+			outputArray = null;
+		var responseXML = processRequest({
+			poNum: poNum,
+			vendorConfig: vendorConfig
+		});
+		
+		vcLog.recordLog({
+			header: 'Response',
+			body: responseXML,
+			transaction: poId
+		});
+		
+		log.debug('process responseXML ' + poNum, responseXML);
+		if (responseXML)
+			outputArray = processResponse({
+				vendorConfig: vendorConfig,
+				responseXML: responseXML
+			});
+		
+		return outputArray;
+	}
+
+    return {
+    	process: process,
+    	processRequest: processRequest,
+    	processResponse: processResponse
+    };
+});
---
+++ varconnect-dev-current/Serial Manipulation/CTC_VC_UE_SerialManipulation.js
@@ -0,0 +1,204 @@
+/**
+ * Copyright (c) 2020 Catalyst Tech Corp
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Catalyst Tech Corp. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Catalyst Tech.
+ *
+ * Project Number: 001225
+ * Script Name: VAR Connect Serial Manipulation UE
+ * Author: paolodl@nscatalyst.com
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ * @Description Runs the corresponding MR script depending on the required process for creating serial numbers
+ *
+ * CHANGELOGS
+ *
+ * Version	Date            Author		    		Remarks
+ * 1.00		May 1, 2020	    paolodl@nscatalyst.com	Initial Build
+ *
+ */
+
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define([
+        'N/task',
+        'N/ui/serverWidget',
+        'N/runtime',
+        'N/record',
+        '../CTC_VC_Constants.js',
+        '../CTC_VC_Lib_MainConfiguration.js',
+        '../CTC_VC_Lib_LicenseValidator',
+        '../Library/CTC_Util.js'],
+function(
+		task,
+		ui,
+		runtime,
+		record,
+		constants,
+		libMainConfig,
+		libLicenseValidator,
+		Util) {
+
+	function _validateLicense(options) {
+		var mainConfig = options.mainConfig,
+			license = mainConfig.license,
+			response = libLicenseValidator.callValidationSuitelet({ license: license,
+				external: true}),
+			result = true;
+
+		if (response == 'invalid') {
+			log.error('License expired', 'License is no longer valid or have expired. Please contact damon@nscatalyst.com to get a new license. Your product has been disabled.');
+			result = false;
+		}
+
+		return result;
+	}
+
+	function _loadMainConfig() {
+		var mainConfig = libMainConfig.getMainConfiguration();
+
+		if (!mainConfig) {
+			log.error('No VAR Connect Main Coniguration available');
+		} else return mainConfig;
+	}
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {string} scriptContext.type - Trigger type
+     * @param {Form} scriptContext.form - Current form
+     * @Since 2015.2
+     */
+    function beforeLoad(scriptContext) {
+    	if (scriptContext.type !== scriptContext.UserEventType.DELETE) {
+    		var mainConfig = _loadMainConfig();
+
+    		//If Serial Scan and Update feature is disabled, hide the corresponding columns
+    		if (!mainConfig.serialScanUpdate ||
+    				!_validateLicense({ mainConfig: mainConfig })) {
+    			var form = scriptContext.form,
+    				sublist = form.getSublist({ id: 'item' });
+
+				if (sublist) {
+    				var field = sublist.getField({
+    					id: constants.Columns.SERIAL_NUMBER_UPDATE
+    				});
+
+	    			//force check if field exists
+	    			if (field && JSON.stringify(field) != '{}') field.updateDisplayType({ displayType: ui.FieldDisplayType.HIDDEN });
+
+					field = sublist.getField({
+						id: constants.Columns.SERIAL_NUMBER_SCAN
+					});
+
+	    			//force check if field exists
+					if (field && JSON.stringify(field) != '{}') field.updateDisplayType({ displayType: ui.FieldDisplayType.HIDDEN });
+				}
+    		}
+    	}
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function afterSubmit(scriptContext) {
+    	if (scriptContext.type !== scriptContext.UserEventType.DELETE &&
+    			runtime.executionContext != runtime.ContextType.MAP_REDUCE) {
+    		log.debug('afterSubmit');
+
+    		var mainConfig = _loadMainConfig();
+
+    		if (mainConfig) {
+		    	var cols = constants.Columns,
+		    		scripts = constants.Scripts.Script,
+		    		hasSerials = false,
+		    		hasNoSerials = false,
+		    		rec = scriptContext.newRecord,
+		    		itemLen = rec.getLineCount({ sublistId: 'item' });
+
+		    	for (var itemCounter=0; itemCounter<itemLen; itemCounter++) {
+		    		var serialString = rec.getSublistValue({
+		        		sublistId: 'item',
+		        		fieldId: cols.SERIAL_NUMBER_SCAN,
+		        		line: itemCounter
+		        	});
+
+		    		var serialStringUpdate = rec.getSublistValue({
+		        		sublistId: 'item',
+		        		fieldId: cols.SERIAL_NUMBER_UPDATE,
+		        		line: itemCounter
+		        	});
+
+
+		    		if ((serialString && serialString.trim()) ||
+		    				(serialStringUpdate && serialStringUpdate.trim())) {
+		    			hasSerials = true;
+		    		} else if ((rec.type == record.Type.ITEM_FULFILLMENT || rec.type == record.Type.INVOICE) &&
+		    				scriptContext.type == scriptContext.UserEventType.CREATE &&
+		    				(!serialStringUpdate || serialStringUpdate.trim().length == 0)) {
+		    			hasNoSerials = true;
+		    		}
+
+		    		if (hasSerials && hasNoSerials)
+		    			break;
+		    	}
+
+		    	//Also check if the corresponding features have been enabled before processing
+		    	if (hasNoSerials &&
+		    			(rec.type == record.Type.INVOICE && mainConfig.copySerialsInv)) {
+					var tranId = rec.getValue({ fieldId: 'tranid' });
+					log.debug(tranId + ' has no serials', true);
+
+					Util.waitRandom(10000);
+
+					var taskOption = {
+					    isMapReduce: true,
+					    scriptId: scripts.SERIAL_UPDATE_ALL_MR,
+					    scriptParams: {},
+					};
+					taskOption.scriptParams['custscript_vc_all_type'] = rec.type;
+					taskOption.scriptParams['custscript_vc_all_id'] = rec.id;
+					var taskId = Util.forceDeploy(taskOption);
+		    	}
+				if (hasSerials &&
+						mainConfig.serialScanUpdate) {
+					var tranId = rec.getValue({ fieldId: 'tranid' });
+					log.debug(tranId + ' has serials', true);
+
+					Util.waitRandom(10000);
+
+					var taskOption = {
+					    isMapReduce: true,
+					    scriptId: scripts.SERIAL_UPDATE_MR,
+					    scriptParams: {},
+					};
+					taskOption.scriptParams['custscript_vc_type'] = rec.type;
+					taskOption.scriptParams['custscript_vc_id'] = rec.id;
+					taskOption.scriptParams['custscript_vc_sender'] = runtime.getCurrentUser().id;
+					var taskId = Util.forceDeploy(taskOption);
+				}
+    		}
+    	}
+    }
+
+    return {
+    	beforeLoad: beforeLoad,
+        afterSubmit: afterSubmit
+    };
+});
